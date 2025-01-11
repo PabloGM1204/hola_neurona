3e51fd8ddf5319658813df608559f21f2e39ade5
PabloGM1204 <pablogm1204@gmail.com>
2025-01-11 12:42:20 +0100

    arreglos de aparencia

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,11 @@ with tab2:
     st.header("Una neurona con dos entradas y dos pesos")
     col1, col2 = st.columns(2)
     with col1:
-        w0 = st.slider("Peso W0", min_value=0.00, max_value=10.0, value=5.0, step=0.01)
-        x0 = st.number_input("Introduzca el valor de la entrada X0", value=0.0)
+        w0 = st.slider("Peso W₀", min_value=0.00, max_value=10.0, value=5.0, step=0.01)
+        x0 = st.number_input("Entrada X₀", value=0.0)
     with col2:
-        w1 = st.slider("Peso W1", min_value=0.00, max_value=10.0, value=5.0, step=0.01)
-        x1 = st.number_input("Introduzca el valor de la entrada X1", value=0.0)
+        w1 = st.slider("Peso W₁", min_value=0.00, max_value=10.0, value=5.0, step=0.01)
+        x1 = st.number_input("Entrada X₁", value=0.0)
 
     if st.button("Calcular la salida", key="calcular_salida_tab2"):
         y = w0 * x0 + w1 * x1
@@ -33,17 +33,17 @@ with tab3:
     st.header("Una neurona con tres entradas, tres pesos y un sesgo")
     col1, col2, col3 = st.columns(3)
     with col1:
-        w0 = st.slider("Peso W0 ", min_value=0.00, max_value=10.0, value=1.0, step=0.01)
-        x0 = st.number_input("Introduzca el valor de la entrada X0 ", value=0.0)
+        w0 = st.slider("Peso W₀ ", min_value=0.00, max_value=10.0, value=1.0, step=0.01)
+        x0 = st.number_input("X₀ ", value=0.0)
     with col2:
-        w1 = st.slider("Peso W1 ", min_value=0.00, max_value=10.0, value=2.0, step=0.01)
-        x1 = st.number_input("Introduzca el valor de la entrada X1 ", value=0.0)
+        w1 = st.slider("Peso W₁ ", min_value=0.00, max_value=10.0, value=2.0, step=0.01)
+        x1 = st.number_input("Entrada X₁ ", value=0.0)
     with col3:
-        w2 = st.slider("Peso W2 ", min_value=0.00, max_value=10.0, value=3.0, step=0.01)
-        x2 = st.number_input("Introduzca el valor de la entrada X2 ", value=0.0)
+        w2 = st.slider("Peso W₂ ", min_value=0.00, max_value=10.0, value=3.0, step=0.01)
+        x2 = st.number_input("Entrada X₂ ", value=0.0)
 
     b = st.number_input("Introduzca el valor del sesgo", value=10.0)
 
     if st.button("Calcular la salida", key="calcular_salida_tab3"):
         y = w0 * x0 + w1 * x1 + w2 * x2 + b
-        st.write("La salida de la neurona es ", y)
+        st.write(f"La salida de la neurona es {y}")
